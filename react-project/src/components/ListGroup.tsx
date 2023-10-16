6ba3290e8ea978e19af86acc850d12c889fab195
Karthik <kkaturi2003@gmail.com>
2023-10-17 01:06:55 +0530

    Rendering

--- react-project/src/components/ListGroup.tsx
+++ react-project/src/components/ListGroup.tsx
@@ -1,9 +1,11 @@
 function ListGroup() {
-  const items = ["New York", "Tokyo", "London", "Paris", "Delhi"];
-
+  let items = ["New York", "Tokyo", "London", "Paris", "Delhi"];
+  items = [];
+ 
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>no item FOUND</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
