97c741a78e55ba6b502880eadd3d65e7d1c9186e
Karthik <kkaturi2003@gmail.com>
2023-11-13 18:40:18 +0530

    CSS Button

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -1,12 +1,27 @@
 import { useState } from "react";
 import ListGroup from "./components/ListGroup/index";
+import { BsFillCalendarFill } from "react-icons/bs";
+import Button from "./components/Button/index";
 function App() {
-    
   let items = ["New York", "Tokyo", "London", "Paris", "Delhi"];
-  const handleSelectItem = (item:string)=>{
-    console.log(item)
-  }
-  return <div><ListGroup onSelectItem={handleSelectItem} items={items} heading="Cities"/></div>
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+  return (
+    <>
+      <BsFillCalendarFill color="red" size="40" />
+      <div>
+        <ListGroup
+          onSelectItem={handleSelectItem}
+          items={items}
+          heading="Cities"
+        />
+      </div>
+      <div>
+        <Button onClick={() => console.log("Clicked")}>My Button </Button>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- react-project/src/components/ListGroup/ListGroup.tsx
+++ react-project/src/components/ListGroup/ListGroup.tsx
@@ -1,47 +1,41 @@
 import { useState } from "react";
 import "./ListGroup.css";
-import styled from "styled-components";
 
-interface ListItemProps {
-  active: boolean;
-}
-
-const List = styled.ul`
-  list-style: none;
-  padding: 0%;
-`;
-const ListItem = styled.li<ListItemProps>`
-  padding: 5px 0;
-  background: ${(props) => (props.active ? "blue" : "none")};
-`;
 interface Props {
-  items: string[];
-  heading: string;
-  onSelectItem: (item: string) => void;
+   items : string[];
+   heading : string;
+   onSelectItem: (item : string) => void;
 }
 
-function ListGroup({ items, heading, onSelectItem }: Props) {
-  const [selectedIndex, setSelectedIndex] = useState(0);
+function ListGroup({items, heading, onSelectItem} : Props) {
+  
+
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  
 
   return (
     <>
       <h1>{heading}</h1>
 
       {items.length === 0 && <p>no item found</p>}
-      <List>
+      <ul className={"list-group  "}>
         {items.map((item, index) => (
-          <ListItem
-            active={index === selectedIndex}
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
             key={item}
             onClick={() => {
-              setSelectedIndex(index);
+              setSelectedIndex( index);
               onSelectItem(item);
             }}
           >
             {item}
-          </ListItem>
+          </li>
         ))}
-      </List>
+      </ul>
     </>
   );
 }
