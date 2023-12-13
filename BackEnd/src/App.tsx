93aaef8980feec3c488a797fa93599b84ad88552
Karthik <kkaturi2003@gmail.com>
2023-12-13 22:33:45 +0530

    creating data

--- BackEnd/src/App.tsx
+++ BackEnd/src/App.tsx
@@ -46,10 +46,24 @@ axios.delete('https://jsonplaceholder.typicode.com/users/'+ user.id)
 })
   }
 
+  const addUser =()=>{
+    const originalUsers = [...users];
+    const newUser ={id:0, name: 'Karthik'};
+    setUsers  ([newUser, ...users]);
+
+    axios.post('https://jsonplaceholder.typicode.com/users', newUser)
+    .then(({data:savedData})=>setUsers([savedData,...users]))
+    .catch (err => {
+      setError(err.message);
+      setUsers(originalUsers);
+
+    });
+  }
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
       {isLoading && <div className="spinner-border"></div>}
+     <button className="btn btn-primary mb-3" onClick={addUser}>Add</button>
       <ul className="list-group">
         {users.map((user) => (
           <li key={user.id} className="list-group-item d-flex justify-content-between">
